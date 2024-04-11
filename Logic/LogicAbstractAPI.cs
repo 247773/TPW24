018161b395ea02b921abe3cad30ec361fb56da4d
Jan Rzepkowski 247777 <jan.rzepkowski21@gmail.com>
2024-04-11 14:44:57 +0200

    interactive and reactive operations in logic layer

--- Logic/LogicAbstractAPI.cs
+++ Logic/LogicAbstractAPI.cs
@@ -12,11 +12,11 @@ namespace Logic
     {
         public static LogicAbstractAPI CreateLogicAPI()
         {
-            return new LogicAPI();
+            return new Table(259, 146); // wymiary stolu bilarodwego typu 8ft
         }
 
-        public abstract void CreateTable(int length, int width, int numOfBalls, int radius);
-
-        public abstract List<Ball> GetBalls();
+        public abstract void CreateBalls(int numOfBalls, int radius); //operacja interaktywna (twórz kule)
+        public abstract void UpdateBalls();
+        public abstract List<(int, int)> GetBallsPosition(); //operacja reaktywna (okresowe wysłanie położenia kul)
     }
 }
--- Logic/Table.cs
+++ Logic/Table.cs
@@ -9,7 +9,7 @@ using Ball = Data.Ball;
 
 namespace Logic
 {
-    internal class Table
+    internal class Table : LogicAbstractAPI
     {
         private int _length;
         private int _width;
@@ -18,20 +18,9 @@ namespace Logic
 
         public Table(int length, int width)
         {
-            _length = length;
-            _width = width;
-            _balls = new List<Ball>();
-        }
-
-        public void createBalls(int numOfBalls, int radius)
-        {
-            for (int i=0; i < numOfBalls; i++)
-            {
-                Random rand = new Random();
-                int x = rand.Next(radius, _length - radius);
-                int y = rand.Next(radius, _width - radius);
-                _balls.Add(new Ball(x, y, radius));
-            }
+            this._length = length;
+            this._width = width;
+            this._balls = new List<Ball>();
         }
 
         public int Length
@@ -47,6 +36,36 @@ namespace Logic
         public List<Ball> Balls
         {
             get { return _balls; }
+        }
+
+        public override void CreateBalls(int numOfBalls, int radius)
+        {
+            for (int i = 0; i < numOfBalls; i++)
+            {
+                Random rand = new Random();
+                int x = rand.Next(radius, _length - radius);
+                int y = rand.Next(radius, _width - radius);
+                _balls.Add(new Ball(x, y, radius));
+            }
+        }
+
+        public override void UpdateBalls()
+        {
+            foreach (Ball ball in _balls)
+            {
+                ball.RandomVelocity(-5, 5);
+                ball.Move();
+            }
+        }
+
+        public override List<(int, int)> GetBallsPosition()
+        {
+            List<(int, int)> positions = new List<(int, int)>();
+            foreach (Ball ball in _balls)
+            {
+                positions.Add((ball.X, ball.Y));
+            }
+            return positions;
         }
     }
 }
