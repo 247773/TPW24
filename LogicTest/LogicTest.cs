ba4979fdaad93319becabf2d9fe1005223dea7fa
Jan Rzepkowski 247777 <jan.rzepkowski21@gmail.com>
2024-05-12 22:37:01 +0200

    Logic layer tests

--- LogicTest/LogicTest.cs
+++ LogicTest/LogicTest.cs
@@ -1,50 +1,89 @@
-/*using Data;
+using Data;
 using Logic;
+using NUnit.Framework;
+using System.Numerics;
+using System.Reflection.Emit;
+using System.Xml.Schema;
 
 namespace LogicTest
 {
-    internal class FakeDataApi : DataAbstractAPI
+    public class TableTests
     {
 
-    }
-
-    public class Tests
-    {
-        private DataAbstractAPI _data;
-        private LogicAbstractAPI _table;
-
-        public void Init()
+        LogicAbstractAPI board = LogicAbstractAPI.CreateAPI(new FakeDataAPI(500, 500));
+        internal class FakeDataBall : IDataBall
         {
-            _data = FakeDataApi.CreateAPI();
-            _table = LogicAbstractAPI.CreateAPI(_data);
+            private Vector2 _position;
+            public override Vector2 Position { get => _position; }
+
+            public override Vector2 Velocity { get; set; }
+            public override bool HasCollided { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public override bool ContinueMoving { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public override event EventHandler<DataEventArgs> ChangedPosition;
+
+            public override void MoveBall()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        internal class FakeDataAPI : IDataTable
+        {
+            public FakeDataAPI(int length, int width)
+            {
+                Length = length;
+                Width = width;
+            }
+
+            public override int Length { get; }
+
+            public override int Width { get; }
+
+
+            public override IDataBall CreateDataBall(int x, int y, int r, int m, int vX, int vY)
+            {
+                return new FakeDataBall();
+            }
+
+            public override List<IDataBall> GetBalls()
+            {
+                return new List<IDataBall>();
+            }
+
+            public override void ClearTable()
+            {
+                return;
+            }
+        }
+
+
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(board);
         }
 
         [Test]
-        public void TestMethod1()
+        public void CreateBallsTest()
         {
-            Init();
-            _table.CreateBalls(5, 10);
-            Assert.AreEqual(5, _table.GetBalls().Count);
+            board.CreateBalls(3, 5);
+            Assert.AreEqual(board.GetBalls().Count, 3);
         }
-
         [Test]
-        public void TestMethod2()
+        public void ClearTableTest()
         {
-            Init();
-            _table.CreateBalls(5, 10);
-            _table.StartSimulation();
-            Assert.AreEqual(5, _table.GetBalls().Count);
+            board.CreateBalls(3, 5);
+            Assert.AreEqual(board.GetBalls().Count, 3);
+
+            board.ClearTable();
+            Assert.AreEqual(board.GetBalls().Count, 0);
         }
-
         [Test]
-        public void TestMethod3()
+        public void ClearingEmptyBoardTest()
         {
-            Init();
-            _table.CreateBalls(5, 10);
-            _table.StartSimulation();
-            _table.ClearTable();
-            Assert.AreEqual(0, _table.GetBalls().Count);
+            board.ClearTable();
+            Assert.AreEqual(board.GetBalls().Count, 0);
         }
     }
 }
-*/
