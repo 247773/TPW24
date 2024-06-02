e9242216d9ef91a3aa669dfb4fe367cfa83ea2b9
freniecki <247773@edu.p.lodz.pl>
2024-06-02 23:11:33 +0200

    data tests - minor adjustments

--- DataTest/DataTest.cs
+++ DataTest/DataTest.cs
@@ -10,16 +10,16 @@ namespace DataTests
         [Test]
         public void CreateBallTest()
         {
-            IDataBall dataBall = table.CreateDataBall(0, 0, 2, 1, 1, 1);
+            IDataBall dataBall = table.CreateDataBall(0, 0, 2, 1, 1, 1, 1);
             Assert.IsNotNull(dataBall);
         }
 
         [Test]
         public void BallVelocityTest()
         {
-            IDataBall dataBall = table.CreateDataBall(1, 2, 3, 4, 5, 6);
-            Vector2 vector = new Vector2(5, 6);
-            Assert.AreEqual(dataBall.Velocity, vector);
+            IDataBall dataBall = table.CreateDataBall(1, 2, 3, 4, 5, 6 ,1);
+            IDataBall.BallVelocity velocity = new IDataBall.BallVelocity(5, 6);
+            Assert.AreEqual(dataBall.Velocity, velocity);
 
         }
 
@@ -27,8 +27,8 @@ namespace DataTests
         public void RemoveBallsTest()
         {
             IDataTable DataAPI = IDataTable.CreateAPI(400, 580);
-            IDataBall dataBall = DataAPI.CreateDataBall(1, 1, 1, 1, 1, 1);
-            IDataBall dataBall2 = DataAPI.CreateDataBall(2, 2, 2, 2, 2, 2);
+            IDataBall dataBall = DataAPI.CreateDataBall(1, 1, 1, 1, 1, 1, 1);
+            IDataBall dataBall2 = DataAPI.CreateDataBall(2, 2, 2, 2, 2, 2, 2);
             Assert.AreEqual(DataAPI.GetBalls().Count, 2);
             DataAPI.ClearTable();
             Assert.AreEqual(DataAPI.GetBalls().Count, 0);
